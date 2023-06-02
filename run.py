d8f06d00c0e27f5c0bda9026874fa1ffeea05d4e
decant09 <>
2023-06-02 18:41:04 +0000

    Amend print statements to address line too long error messages

--- run.py
+++ run.py
@@ -11,14 +11,18 @@ username = ""
 
 def intro():
     print(art)
-    print("Welcome to Machu Picchu, an adventure based game where you hike to the ancient and magical summit of Machu Picchu.")
-    print("Your story begins as you are travelling through South America and decide to visit the iconic ruined city of the Incas.")
+    print("Welcome to Machu Picchu, an adventure based game where you hike to the ancient")
+    print("and magical summit of Machu Picchu.")
+    print("Your story begins as you are travelling through South America and decide to")
+    print("visit the iconic ruined city of the Incas.")
     print("Your guide Miguel will accompany you on your Incan journey.\n")
     
 
 def miguel():
-    print("You have just met Miguel your guide. He speaks some English, more than you do Spanish.") 
-    print("He seems friendly, knowledgeable and can trace his ancestry to the lost Incan civilization.\n")
+    print("You have just met Miguel your guide. He speaks some English, more than you")
+    print("speak Spanish.") 
+    print("He seems friendly, knowledgeable and can trace his ancestry to the lost Incan")
+    print(" civilization.\n")
     option = input("Would you like to continue on your adventure with Miguel? Y or N:\n>>> ").lower().strip()
     if option == 'y':
         print(f"Great {username}!\n")
@@ -35,10 +39,12 @@ def duration():
     print("Miguel explains to you that you can complete the hike in either 3 or 5 days.")
     option = int(input("How many days would you prfer to spend hiking? 3 or 5:\n>>> "))
     if option == 3:
-        print(f"Wow {username}, you must be up for the challenge! Hiking to an altitude of 2430m in 3 days is no joke!\n")
+        print(f"Wow {username}, you must be up for the challenge! Hiking to an altitude")
+        print("of 2430m in 3 days is no joke!\n")
         three_days()
     elif option == 5:
-        print(f"You're taking a more relaxed approcah {username} with opting for 5 days! You are on holiday afterall!\n")
+        print(f"You're taking a more relaxed approcah {username} with opting for 5 days!")
+        print("You are on holiday afterall!\n")
         five_days()
         # use a quit()
     else:
@@ -47,8 +53,10 @@ def duration():
 
 
 def three_days():
-    print("The hike is going well but Miguel thinks that you won't make the summit in 3 days.")
-    print("He suggests a shortcut which involves diving into a river from on top of a bridge.")
+    print("The hike is going well but Miguel thinks that you won't make the summit in")
+    print("3 days.")
+    print("He suggests a shortcut which involves diving into a river from on top of a")
+    print("bridge.")
     option = input("Do you take Miguel's advice and dive into the river? Y or N:\n>>> ").lower().strip()
     if option == 'y':
         print(f"Oh no {username}, that river was treacherous!")
@@ -94,7 +102,7 @@ while True:
     try:
         username = str(input("Please enter your name:\n>>> ")).capitalize().strip()
         if len(username) <= 2 or not username.isalpha():
-            raise ValueError("Username must not have any spaces, be at least 3 characters long, and only contain letters.")
+            raise ValueError("Username must not have any spaces, be at least 3 characters long,\nand only contain letters.")
             continue
     except ValueError as e:
         print(f"Invalid Entry: {e}")
